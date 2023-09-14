71a6769813fdffe5708204c11d00bc8ef8a4b768
MarvinCrepin <marvincrepin@gmail.com>
2023-09-14 11:24:17 +0200

    test: e2e 80%

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,27 +1,52 @@
-describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login')
+import { adminUser } from "./samples/samples";
+
+describe('Login', () => {
+  it('should login successfully', () => {
+    cy.visit('/login');
 
     cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
+      body: adminUser,
+    });
 
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
+    cy.intercept('GET', '/api/session', []).as('session');
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-
-    cy.url().should('include', '/sessions')
+    cy.get('input[formControlName=email]').type("yoga@studio.com");
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`);
+    cy.url().should('include', '/sessions');
   })
+
+  it('should logout successfully', () => {
+    cy.url().should('include', '/sessions');
+    cy.get('.mat-toolbar > .ng-star-inserted > :nth-child(3)').click();
+    cy.url().should('not.contain', '/sessions');
+  });
+
+
+  it('should disable submit button if email field is empty', () => {
+    cy.visit('/login');
+    cy.get('input[formControlName=email]').clear;
+    cy.get('input[formControlName=password]').type(`${"wrongpass"}{enter}{enter}`);
+    cy.get('input[formControlName=email]').should('have.class', 'ng-invalid');
+    cy.get('.error').should('be.visible');
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
+
+  it('should not login with bad credentials', () => {
+    cy.visit('/login');
+    cy.get('input[formControlName=email]').type("yoga@studio.com");
+    cy.get('input[formControlName=password]').type(`${"test!4321"}{enter}{enter}`);
+    cy.url().should('not.contain', '/sessions');
+    cy.get('.error').should('be.visible');
+  });
+
+  it('should disable submit button if password field is empty', () => {
+    cy.visit('/login');
+    cy.get('input[formControlName=password]').clear;
+    cy.get('input[formControlName=email]').type(`${"yoga@studio.com"}{enter}{enter}`);
+    cy.get('input[formControlName=password]').should('have.class', 'ng-invalid');
+    cy.get('.error').should('be.visible');
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+
 });
---
+++ front/cypress/e2e/register.cy.ts
@@ -0,0 +1,42 @@
+
+describe('Register', () => {
+    it('register successfully', () => {
+        cy.visit('/register')
+
+        cy.intercept('POST', '/api/auth/register', { statusCode: 200 })
+
+        cy.get('input[formControlName=firstName]').type("John")
+        cy.get('input[formControlName=lastName]').type("Doe")
+        cy.get('input[formControlName=email]').type("john@doe.com")
+        cy.get('input[formControlName=password]').type(`${"john123"}{enter}{enter}`)
+        cy.url().should('include', '/login')
+    })
+
+
+    it('should show an error if email is already used', () => {
+        cy.visit('/register')
+
+        cy.intercept('POST', '/api/auth/register', { statusCode: 400 })
+
+        cy.get('input[formControlName=firstName]').type("firstname")
+        cy.get('input[formControlName=lastName]').type("lastname")
+        cy.get('input[formControlName=email]').type("john@doe.com")
+        cy.get('input[formControlName=password]').type(`${"john123"}{enter}{enter}`)
+        cy.get('.error').should('be.visible');
+    })
+
+
+    it('should disable submit button if email field is empty', () => {
+        cy.visit('/register')
+
+        cy.get('input[formControlName=firstName]').type("John")
+        cy.get('input[formControlName=lastName]').type("Doe")
+        cy.get('input[formControlName=password]').type(`${"john123"}{enter}{enter}`)
+        cy.get('input[formControlName=email]').should('have.class', 'ng-invalid')
+        cy.get('button[type=submit]').should('be.disabled')
+    })
+
+
+
+
+})
